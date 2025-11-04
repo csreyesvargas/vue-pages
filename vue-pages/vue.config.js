7b3fd79b56b25313da081708bd788e51d3dfdd5a
csreyesvargas <csreyesvargas@my.waketech.edu>
2025-11-03 22:22:11 -0500

    Update vue.config.js

--- vue-pages/vue.config.js
+++ vue-pages/vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: "/vue-pages/vue-pages/"
+  publicPath: "/vue-pages/"
 })
